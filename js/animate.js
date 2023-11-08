ba1fb260aef012df7161f8a8b96861b128f6b3e9
Dmitry Pavlov <dmitriiy.pavlov@gmail.com>
2023-11-08 09:33:52 +0300

    add overflowY for acor__list

--- js/animate.js
+++ js/animate.js
@@ -2,7 +2,7 @@ const tl = new TimelineMax();
 tl.from(".first__advantages-list", 1, {translateY: -400,opacity: 0,scale: 1,})
 tl.to(".first__advantages-item", 1, {width:"fit-content",})
 tl.from(".first__heading-group", 1, {opacity: 0,})
-tl.to(".first__advantages-item1", 1, {translateY: -21,translateX: -7,opacity: 1,scale: 1,rotation: -30})
+tl.to(".first__advantages-item1", 1, {rotationX:180,rotationY:180,translateY: -21,translateX: -7,opacity: 1,scale: 1,rotation: -30,})
 tl.from(".last__descr-list", 0.3, {translateY: -400,opacity: 0,})
 tl.from(".first__text", 1, {opacity: 0,})
 
