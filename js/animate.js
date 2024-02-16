7ae2c9581a97161510845ebdbe617639e11d0a46
Dmitry Pavlov <dmitriiy.pavlov@gmail.com>
2024-02-16 11:52:47 +0300

    change animate

--- js/animate.js
+++ js/animate.js
@@ -1,9 +1,9 @@
 const tl = new TimelineMax();
 tl.from(".first__advantages-list", 1, {translateY: -400,opacity: 0,scale: 1,})
-tl.to(".first__advantages-item", 1, {width:"fit-content",})
+tl.to(".first__advantages-item", 1, {width:"fit-content"})
+tl.to(".first__advantages-item1", 1, {translateY: -40,translateX: -12,opacity: 1,scale: 1,rotation: -30,})
 tl.from(".first__heading-group", 1, {opacity: 0,})
-tl.to(".first__advantages-item1", 1, {rotationX:180,rotationY:180,translateY: -21,translateX: -7,opacity: 1,scale: 1,rotation: -30,})
-tl.from(".last__descr-list", 0.3, {translateY: -400,opacity: 0,})
+tl.from(".last__descr-list", 1, {translateY: -400,opacity: 0,})
 tl.from(".first__text", 1, {opacity: 0,})
 
   
